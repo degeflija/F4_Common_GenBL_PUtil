8368f1642ffe0c108cff05756655b3d2ed118a4f
Horst <horst@rupp-family.de>
2026-09-25 17:57:29 +0200

    V7.22.22: ReBoot auf P_Util PE7
    Auto-commit: 2 file(s) in Core_Src
    
    Timestamp: 2026-09-25 17:57
    
    Changed files:
      [modified] Core_Src/task_SystemTimer.c
      [modified] Core_Src/task_SystemTimer.h

--- Core_Src/task_SystemTimer.c
+++ Core_Src/task_SystemTimer.c
@@ -22,8 +22,43 @@
 //
 // *****************************************************************************
 //
+//  25.09.2026 : Boot-Taster ( Beschreibung siehe task_SystemTimer.h )
+//
+void Boot_Button_Init ( void )
+{
+  GPIO_InitTypeDef l_init = { 0 };
+
+  __HAL_RCC_GPIOE_CLK_ENABLE ( );
+
+  l_init.Pin    = Boot_Button_Ext_Pin;
+  l_init.Mode   = GPIO_MODE_INPUT;
+  l_init.Pull   = GPIO_PULLUP;          //  offen = 1, gedrueckt ( gegen GND ) = 0
+  l_init.Speed  = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init ( Boot_Button_Ext_Port, &l_init );
+}
+
+//
+//  true, wenn B1 ( PA0, gedrueckt = 1 ) ODER der externe Taster
+//  ( PE7, gedrueckt = 0 ) gedrueckt ist. Ohne Entprellung.
+//
+uint8_t Boot_Button_Pressed ( void )
+{
+  return  (
+            ( HAL_GPIO_ReadPin ( B1_GPIO_Port, B1_Pin ) == GPIO_PIN_SET )
+          ||
+            ( HAL_GPIO_ReadPin ( Boot_Button_Ext_Port, Boot_Button_Ext_Pin ) == GPIO_PIN_RESET )
+          );
+}
+
+//
+// *****************************************************************************
+//
 void SystemTimer ( void * nix)
 {
+  uint8_t   l_boot_armed    = false;   //  erst nach einmal Loslassen
+  uint8_t   l_boot_count    = 0;
+
+  Boot_Button_Init ( );                //  25.09.2026
 
   #ifdef LED_System_Timer
   Initialize_Flash_Sequence ( LED_System_Timer,
@@ -52,10 +87,20 @@ void SystemTimer ( void * nix)
     // ========================================================================
     // ========================================================================
     //
-    //  Blue Button
+    //  Boot-Taster : blauer Taster B1 oder externer Taster an PE7
+    //  ( 25.09.2026, siehe task_SystemTimer.h )
     //
-    uint8_t l_button_pattern =  HAL_GPIO_ReadPin ( B1_GPIO_Port, B1_Pin );
-    if ( l_button_pattern )
+    if ( ! Boot_Button_Pressed ( ) )
+    {
+      l_boot_armed  = true;
+      l_boot_count  = 0;
+    }
+    else if ( l_boot_armed && ( l_boot_count < c_Boot_Button_Debounce ) )
+    {
+      l_boot_count++;
+    }
+
+    if ( l_boot_armed && ( l_boot_count >= c_Boot_Button_Debounce ) )
     {
       //F4_Clear_Signature_Sector_7 ();
       Bootloader_JumpToColdStart ();      // Good Luck !!
